234ecc075404afe7d1597e76fb6a8bdcde27ff00
Dev Shah <busycaesar@gmail.com>
2024-01-14 15:10:22 -0500

    Replaced console.log with logger.info!

--- src/server.js
+++ src/server.js
@@ -8,10 +8,12 @@ const PORT = parseInt(process.env.PORT || '8080', 10);
 
 // Initiate the server to listen on the port!
 const server = stoppable(
-  app.listen(PORT, () =>
+  app.listen(PORT, () => {
     // Logging a message when the server starts listening!
-    logger.info(`Server is listening on PORT ${PORT}`)
-  )
+    logger.info(`Server is listening on PORT ${PORT}`);
+    // Logging all the environment variables if the LOG_LEVEL is debug!
+    if (process.env.LOG_LEVEL === 'debug') logger.info(process.env);
+  })
 );
 
 // Exporting the server for other parts of the code if it is require!
