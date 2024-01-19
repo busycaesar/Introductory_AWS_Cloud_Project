32af011b3f8f55d54a98c6e6d036e333c8f5f433
Dev Shah <busycaesar@gmail.com>
2024-01-19 07:27:38 -0500

    Updated the 404 route to use router.get instead of app.get!

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 
 // Information about version and author from package.json!
-const {version, author} = require('../../package.json');
+const { version, author } = require('../../package.json');
 
 // Creating a router to mount APIs!
 const router = express.Router();
@@ -13,19 +13,19 @@ router.use('/v1', require('./api'));
 
 // A simple health check router to check if the server is still working!
 router.get('/', (req, res) => {
-	// Making sure that client dont cache this response!
-	res.setHeader('Cache-Control','no-cache');
+  // Making sure that client dont cache this response!
+  res.setHeader('Cache-Control', 'no-cache');
 
-	res.status(200).json({
-		status:'Okay!',
-		author,
-		githubUrl: 'https://github.com/busycaesar/fragments',
-		version,
-	});
+  res.status(200).json({
+    status: 'Okay!',
+    author,
+    githubUrl: 'https://github.com/busycaesar/fragments',
+    version,
+  });
 });
 
 // 404 middleware to handle responses for unavailable resources!
-app.use((req, res) => {
+router.use((req, res) => {
   res.status(404).json({
     status: 'Error!',
     error: {
@@ -33,19 +33,6 @@ app.use((req, res) => {
       code: 404,
     },
   });
-});
-
-// Error handling middleware!
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  // Using the status and message from the error object if it is there, otherwise using a generic server error status and message!
-  const status = err.status || 500,
-    message = err.message || 'Unable to process the request!';
-
-  // Logging something specific so we know if there is any server error!
-  if (status > 499) logger.error({ err }, 'Error processing request!');
-
-  res.status(status).json({ status: 'Error!', error: { message, code: status } });
 });
 
 module.exports = router;
