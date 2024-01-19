a2fbbd03b3406f307bdadc61e93905cba77c6005
Dev Shah <busycaesar@gmail.com>
2024-01-18 19:00:41 -0500

    Updated index.js file by adding comments!

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ require('dotenv').config();
 // Helps to log any crash/information required/vital while debugging!
 const logger = require('./logger');
 
-// If the app crashes due to an uncaught exception, it will be logged first.
+// If the app crashes due to an uncaught exception, it will be logged first!
 process.on('uncaughtException',(err, origin)=>{
 	logger.fatal({err, origin}, 'uncaughtException');
 	throw err;
 });
 
+// If the app crashes due to unhandled promise rejection, it will be logged first!
 process.on('unhandledRejection',(reason, promise)=>{
 	logger.fatal({reason, origin},'unhandledRejection');
 	throw reason;
