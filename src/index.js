4153a42092a99869036c7433b69ad8e6ac78bbaf
Dev Shah <busycaesar@gmail.com>
2024-01-19 07:28:37 -0500

    Updated code format using prettier!

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 // src/index.js
 
-// This helps to read variables from .env file! 
+// This helps to read variables from .env file!
 require('dotenv').config();
 
 // Helps to log any crash/information required/vital while debugging!
 const logger = require('./logger');
 
 // If the app crashes due to an uncaught exception, it will be logged first!
-process.on('uncaughtException',(err, origin)=>{
-	logger.fatal({err, origin}, 'uncaughtException');
-	throw err;
+process.on('uncaughtException', (err, origin) => {
+  logger.fatal({ err, origin }, 'uncaughtException');
+  throw err;
 });
 
 // If the app crashes due to unhandled promise rejection, it will be logged first!
-process.on('unhandledRejection',(reason, promise)=>{
-	logger.fatal({reason, origin},'unhandledRejection');
-	throw reason;
+process.on('unhandledRejection', (reason, promise) => {
+  logger.fatal({ reason, promise }, 'unhandledRejection');
+  throw reason;
 });
 
 require('./server');
