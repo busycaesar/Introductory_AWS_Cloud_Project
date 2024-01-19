75839c506811c98149f9655bfce5f60aeeedeeb9
Dev Shah <busycaesar@gmail.com>
2024-01-19 07:24:52 -0500

    Moved the error handleing middleware to src/app.js from src/routes/index.js!

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
-// Importing required libraries and files!
+// src/app.js
+
+// # Importing required libraries and files!
 
 const express = require('express');
 const cors = require('cors');
@@ -10,15 +12,36 @@ const pino = require('pino-http')({ logger });
 
 const { author, version } = require('../package.json');
 
+const passport = require('passport');
+const authenticate = require('./auth');
+
 const app = express();
 
-// Middlewares
+// # Middlewares
 
 app.use(pino);
 app.use(helmet());
 app.use(cors());
 app.use(compression());
-app.use('/',require('./routes'))
 
-// Exporting the app to access it in server.js
+// ## Setting up the passport authentication middleware!
+passport.use(authenticate.strategy());
+app.use(passport.initialize());
+
+app.use('/', require('./routes'));
+
+// ## Error handling middleware!
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  // Using the status and message from the error object if it is there, otherwise using a generic server error status and message!
+  const status = err.status || 500,
+    message = err.message || 'Unable to process the request!';
+
+  // Logging something specific so we know if there is any server error!
+  if (status > 499) logger.error({ err }, 'Error processing request!');
+
+  res.status(status).json({ status: 'Error!', error: { message, code: status } });
+});
+
+// # Exporting the app to access it in server.js
 module.exports = app;
