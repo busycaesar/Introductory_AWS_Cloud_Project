7ec545c9807e2e625b0fa000a34327d18a021016
Dev Shah <busycaesar@gmail.com>
2024-01-25 14:09:52 -0500

    Added comments to explain the code!

--- jest.config.js
+++ jest.config.js
@@ -5,12 +5,15 @@ const path = require("path");
 const envFile = path.join(__dirname, "env.jest");
 
 // Reading the environment variables!
+// It is important to pass the path to the enviroment file since we have a special custom file to store the environment variables for jest vs .env which we use to store the information used in the server.js file!
 require("dotenv").config({ path: envFile });
 
 // Logging the information for the developers!
 console.log(`Currently using LOG_LEVEL=${process.env.LOG_LEVEL}. Use 'debug' in env.jest for see more details!`);
 
 module.exports = {
+	// Verbose: true makes the jest output more detailed information during the test runs!
 	verbose: true,
+	// If a test takes more than 5 seconds, it is considered is already failed!
 	testTimeOut: 5000,
 };
