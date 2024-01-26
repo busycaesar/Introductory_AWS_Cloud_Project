c2c58dc2e13749a843300f33725af35d450d8c81
Dev Shah <busycaesar@gmail.com>
2024-01-26 10:02:20 -0500

    Updated the format of the auth files!

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,11 +1,10 @@
 // src/auth/index.js
 
 // Exporting amazon cognito
-if(process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID){
-	module.exports = require("./cognito");
+if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
+  module.exports = require('./cognito');
 }
 // Making sure to export the basic authentication only in case that the project is not in production!
-else if(process.env.HTPASSWD_FILE && process.NODE_ENV != "production"){
-	module.exports = require("./basic-auth");
-}
-else throw new Error("Missing env variables: No authorization configuration found!");
+else if (process.env.HTPASSWD_FILE && process.NODE_ENV != 'production') {
+  module.exports = require('./basic-auth');
+} else throw new Error('Missing env variables: No authorization configuration found!');
