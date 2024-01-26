30ef56e2a27227e1b222b8b0446bf27389754f32
Dev Shah <busycaesar@gmail.com>
2024-01-26 10:10:56 -0500

    Added the test file for checking the get route of /v1/fragments!

---
+++ tests/unit/get.test.js
@@ -0,0 +1,27 @@
+// tests/unit/get.test.js
+
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('GET /v1/fragments', () => {
+  
+  // Making sure for all the unauthorized requests, the status code of 401 is sent!
+  test('unauthenticated requests are denied', () => request(app).get('/v1/fragments').expect(401));
+
+  // Checking the credentials for all the authorized requests! 
+  test('incorrect credentials are denied', () =>
+    // Sending an invalid email and password using the auth subfunction of request function!
+    request(app).get('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
+
+  // Using a valid username/password pair should give a success result with a .fragments array
+  test('authenticated users get a fragments array', async () => {
+    const response = await request(app).get('/v1/fragments').auth('user1@email.com', 'password1');
+    expect(response.statusCode).toBe(200);
+    expect(response.body.status).toBe('Okay!');
+    // Making sure that the data of Array type is returned in the response!
+    expect(Array.isArray(res.body.fragments)).toBe(true);
+  });
+
+  // TODO: we'll need to add tests to check the contents of the fragments array later
+});
