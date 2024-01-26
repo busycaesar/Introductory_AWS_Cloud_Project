5259db0e8b5b13b50b86f46ede88376ea624dd3b
Dev Shah <busycaesar@gmail.com>
2024-01-26 10:51:36 -0500

    Updated test file for checking 404 routes!

--- tests/unit/routesIndex.test.js
+++ tests/unit/routesIndex.test.js
@@ -7,14 +7,14 @@ const app = require("../../src/app");
 describe("Check 404 Error", () => {
 
 	// Making sure for all unexpected path, 401 status code is being sent!
-	test("Correct status code is sent", () => {
-		request(app).get("/non-existing-path").expect(404);
+	test("Correct status code is sent", async () => {
+		await request(app).get("/non-existing-path").expect(404);
 	});
 
 	// Checking the error message for all the unexpected path!
 	test("Checking error object", async () => {
 		const response = await request(app).get("/non-existing-path");
-		const resBody = await response.body;
+		const resBody = response.body;
 		expect(resBody.status).toBe("Error!");
 		expect(resBody.error.message).toBe("Not Found!");
 		expect(resBody.error.code).toBe(404);
