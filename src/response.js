f0a6cdda26610b17fecbe3de4d2e84d04c59cbf2
Dev Shah <busycaesar@gmail.com>
2024-02-03 09:12:35 -0500

    Added comments to the response.js to explain the propose of its existance!

--- src/response.js
+++ src/response.js
@@ -1,5 +1,7 @@
 // src/response.js
 
+// This file has function which can be used to sent the response. This functions are helpful because it contains all the required const data to be sent in both the cases: Error and Success!
+
 module.exports.createSuccessResponse = (data) => {
   return {
     status: 'Okay!',
