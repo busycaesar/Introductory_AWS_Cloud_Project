bec38bef80c3c70579991eb62c476296daa32c5b
Dev Shah <busycaesar@gmail.com>
2024-02-03 12:13:00 -0500

    Created test files for http methods!

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -5,11 +5,10 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 describe('GET /v1/fragments', () => {
-  
   // Making sure for all the unauthorized requests, the status code of 401 is sent!
   test('unauthenticated requests are denied', () => request(app).get('/v1/fragments').expect(401));
 
-  // Checking the credentials for all the authorized requests! 
+  // Checking the credentials for all the authorized requests!
   test('incorrect credentials are denied', () =>
     // Sending an invalid email and password using the auth subfunction of request function!
     request(app).get('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
@@ -22,6 +21,4 @@ describe('GET /v1/fragments', () => {
     // Making sure that the data of Array type is returned in the response!
     expect(Array.isArray(response.body.fragments)).toBe(true);
   });
-
-  // TODO: we'll need to add tests to check the contents of the fragments array later
 });
