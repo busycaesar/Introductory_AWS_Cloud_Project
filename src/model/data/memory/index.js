07f1158fb1a5aabc2393d4842530f3369d0d0746
Dev Shah <busycaesar@gmail.com>
2024-02-05 08:48:38 -0500

    Added index.js for memory containing functions to store the data using the memoryDB class!

---
+++ src/model/data/memory/index.js
@@ -0,0 +1,48 @@
+const MemoryDB = require('./memory-db');
+
+// Create two in-memory databases: one for fragment metadata and the other for raw data
+const data = new MemoryDB();
+const metadata = new MemoryDB();
+
+// Write a fragment's metadata to memory db. Returns a Promise
+const writeFragment = (fragment) => metadata.put(fragment.ownerId, fragment.id, fragment);
+
+// Read a fragment's metadata from memory db. Returns a Promise
+const readFragment = (ownerId, id) => metadata.get(ownerId, id);
+
+// Write a fragment's data buffer to memory db. Returns a Promise
+const writeFragmentData = (ownerId, id, buffer) => data.put(ownerId, id, buffer);
+
+// Read a fragment's data from memory db. Returns a Promise
+const readFragmentData = (ownerId, id) => data.get(ownerId, id);
+
+// Get a list of fragment ids/objects for the given user from memory db. Returns a Promise
+const listFragments = async (ownerId, expand = false) => {
+  const fragments = await metadata.query(ownerId);
+
+  // If we don't get anything back, or are supposed to give expanded fragments, return
+  if (expand || !fragments) {
+    return fragments;
+  }
+
+  // Otherwise, map to only send back the ids
+  return fragments.map((fragment) => fragment.id);
+};
+
+// Delete a fragment's metadata and data from memory db. Returns a Promise
+const deleteFragment = (ownerId, id) =>
+  Promise.all([
+    // Delete metadata
+    metadata.del(ownerId, id),
+    // Delete data
+    data.del(ownerId, id),
+  ]);
+
+module.exports = {
+  listFragments,
+  writeFragment,
+  readFragment,
+  writeFragmentData,
+  readFragmentData,
+  deleteFragment,
+};
