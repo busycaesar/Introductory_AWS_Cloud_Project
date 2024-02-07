3122de060b62030ee7baf3598a2f74a5850783ca
Dev Shah <busycaesar@gmail.com>
2024-02-06 20:01:10 -0500

    Added tests for checking fragments class!

---
+++ tests/unit/fragment.test.js
@@ -0,0 +1,82 @@
+// tests/unit/fragment.test.js
+
+const Fragment = require('../../src/model/fragment');
+
+describe('Fragment Class', () => {
+  test('Checking the constructor and getFragment!', async () => {
+    // Creating a new object using construction!
+    const _fragment = new Fragment({ id: 'dsa', ownerId: 'asd', type: 'text' });
+    // Saving the created object!
+    _fragment.save();
+    // Trying the fetch the stored fragment using the fragment id and owner id!
+    const _storedFragment = await Fragment.getFragment('asd', 'dsa');
+    // Checking if the id received is the same and the one stored!
+    expect(_storedFragment.id).toBe('dsa');
+  });
+
+  test('Checking the getAllFragments functionality!', async () => {
+    // Creating new metaData to be stored as a fragment metadata.
+    const metaData = { id: 'dsa', ownerId: 'wsc', type: 'text' },
+      data2 = { id: 'dsa2', ownerId: 'wsc', type: 'text' };
+    // Creating new objects using construction!
+    const _fragment = new Fragment(metaData),
+      _fragment2 = new Fragment(data2);
+    // Saving the created objects!
+    _fragment.save();
+    _fragment2.save();
+    // Trying the fetch all the stored fragments of the owner using the owner id!
+    const _allFragments = await Fragment.getAllFragments('wsc');
+    // Making sure that the getAllFragments returns an array!
+    expect(Array.isArray(_allFragments)).toBe(true);
+    // Making sure that the length of the array is two since two metadata is stored!
+    expect(_allFragments.length).toBe(2);
+    // Checking if the id received is the same and the one stored!
+    expect(_allFragments[0]).toBe('dsa');
+    expect(_allFragments[1]).toBe('dsa2');
+    // Getting all the fragments of the owner along with its details!
+    const _allFragmentsWithDetail = await Fragment.getAllFragments('wsc', true);
+    // Making sure the object received has all the properties of the metaData stored.
+    expect(_allFragmentsWithDetail[0]).toEqual(expect.objectContaining(metaData));
+    expect(_allFragmentsWithDetail[1]).toEqual(expect.objectContaining(data2));
+  });
+
+  test('Checking the delete functionality!', async () => {
+    // Creating a new metaData to be stored as a fragment metadata.
+    const metaData = { id: 'dsa', ownerId: 'qen#', type: 'text' };
+    // Initiating a new fragment object and passing the metaData.
+    const _fragment = new Fragment(metaData);
+    // Saving the fragment object to store the metaData.
+    await _fragment.save();
+    await _fragment.setData({});
+    // Deleting the fragment using its id and owner id!
+    await Fragment.delete('qen#', 'dsa');
+    // Getting the fragment object from the stored fragment metaData by passing the fragment id and owner id!
+    const _storedFragment = await Fragment.getFragment('qen#', 'dsa');
+    // Making sure the returned stored fragment object is undefined since the fragment is already deleted!
+    expect(_storedFragment).toEqual(undefined);
+  });
+
+  test('Storing the fragment data!', async () => {
+    // Creating a new metaData to be stored as a fragment metadata.
+    const metaData = { id: 'dsa', ownerId: 'aus', type: 'text' },
+      fragmentData = {
+        name: 'Dev',
+        designation: 'Developer',
+        location: 'Toronto, ON',
+      };
+    // Initiating a new fragment object and passing the metaData.
+    const _fragment = new Fragment(metaData);
+    // Saving the fragment object to store the metaData.
+    await _fragment.save();
+    await _fragment.setData(fragmentData);
+    const _storedFragmentData = await _fragment.getData();
+    expect(_storedFragmentData).toEqual(fragmentData);
+  });
+
+  test('format testing', () => {
+    const metaData = { id: 'dsa', ownerId: 'aus', type: 'text/html; charset=utf-8' };
+    const _fragment = new Fragment(metaData);
+    expect(_fragment.mimeType).toBe('text/html');
+    expect(_fragment.isText()).toBe(true);
+  });
+});
