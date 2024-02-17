476ff541584e29d19de577eaa953c760c9c7e547
Dev Shah <busycaesar@gmail.com>
2024-02-17 10:05:14 -0500

    Added a test case to make sure GET requests returns an array of the same id stored!

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -2,6 +2,8 @@
 
 const request = require('supertest');
 const app = require('../../src/app');
+const Fragment = require('../../src/model/fragment');
+const hash = require('../../src/hash');
 
 describe('GET /v1/fragments', () => {
   // Making sure for all the unauthorized requests, the status code of 401 is sent!
@@ -21,5 +23,31 @@ describe('GET /v1/fragments', () => {
     expect(response.body.status).toBe('Okay!');
     // Making sure that the data of Array type is returned in the response!
     expect(Array.isArray(response.body.fragments)).toBe(true);
+  });
+
+  // Using a valid username/password pair should give a success result with a .fragments array
+  test('authenticated users gets an array of correct fragments id', async () => {
+    // Create fragments
+    const hashedOwnerId = hash('user1@email.com');
+    const fragMetadata1 = new Fragment({ id: 'rdmId', ownerId: hashedOwnerId, type: 'text/plain' }),
+      fragMetadata2 = new Fragment({ id: 'rdmId2', ownerId: hashedOwnerId, type: 'text/plain' });
+    // Store the fragments for the authenticated user!
+    fragMetadata1.save();
+    fragMetadata2.save();
+    // Request the fragments for the autheticated user!
+    // Storing the response of the get request along with passing true credentials!
+    const response = await request(app).get('/v1/fragments').auth('user1@email.com', 'ps1');
+    // Make sure the returned array is the same id as the stored ones!
+    expect(response.body.fragments[0]).toBe('rdmId');
+    expect(response.body.fragments[1]).toBe('rdmId2');
+  });
+});
+
+describe('GET /v1/fragments/?expand=1', () => {
+  test('Whole metadata is returned with expand query', async () => {
+    // Storing the response of the get request along with passing true credentials!
+    const response = await request(app)
+      .get('/v1/fragments/?expand=1')
+      .auth('user1@email.com', 'ps1');
   });
 });
