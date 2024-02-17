88a3cf93ef4eda748f3467312998b21660daf17f
Dev Shah <busycaesar@gmail.com>
2024-02-17 14:05:53 -0500

    test(GET API) Add unit test for new GET API returning information about the specific fragment

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -64,3 +64,44 @@ describe('GET /v1/fragments/?expand=1', () => {
     expect(response.body.fragments[1].ownerId).toBe(hashedOwnerId);
   });
 });
+
+describe('GET /v1/fragments/:id/info', () => {
+  const path = (id = 123) => `/v1/fragments/${id}/info`;
+  // Making sure for all the unauthorized requests, the status code of 401 is sent!
+  test('unauthenticated requests are denied', () => request(app).get(path()).expect(401));
+
+  // Checking the credentials for all the authorized requests!
+  test('incorrect credentials are denied', () =>
+    // Sending an invalid email and password using the auth subfunction of request function!
+    request(app).get(path()).auth('invalid@email.com', 'incorrect_password').expect(401));
+
+  // Authenticated user should get 404 if the fragment associated with the id passed doesnt exists!
+  test('authenticated user passing the incorrect fragment id receive 404', async () => {
+    // Passing the correct credentials with incorrect fragment id!
+    const response = await request(app).get(path()).auth('user1@email.com', 'ps1');
+    // Making sure it returns 404 since the id is incorrect!
+    expect(response.status).toBe(404);
+  });
+
+  // Authenticated user should get 404 if the fragment associated with the id passed doesnt exists!
+  test('authenticated user passing the incorrect fragment id receive 404', async () => {
+    // Getting the hash value of the owner's email!
+    const hashId = hash('user1@email.com');
+    // Creating the sample fragment metadata!
+    const fragMetadata = new Fragment({
+      id: 'dev',
+      ownerId: hashId,
+      type: 'text/plain',
+    });
+    // Storing the fragment metadata!
+    fragMetadata.save();
+    // Passing the correct credentials with incorrect fragment id!
+    const response = await request(app).get(path('dev')).auth('user1@email.com', 'ps1');
+    // Making sure it returns 404 since the id is incorrect!
+    expect(response.status).toBe(201);
+    expect(response.body.status).toBe('Okay!');
+    expect(response.body.fragment.id).toBe('dev');
+    expect(response.body.fragment.ownerId).toBe(hashId);
+  });
+  // Authenticated use should get the fragment if the fragment with the id passed exists.
+});
