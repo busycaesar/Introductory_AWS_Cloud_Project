2d74a5c3e871af87a0af755b0cd9b176e7567ab4
Dev Shah <busycaesar@gmail.com>
2024-03-11 17:55:57 -0400

    Add(API): Update API to send the fragment data in required type

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -6,54 +6,74 @@ const Fragment = require('../../model/fragment');
 
 // This API sends all the fragments of the user in an array!
 const getFragments = async (req, res) => {
+  // Get the owner id from the request object.
+  const ownerId = req.user;
   // If the requst has a query "expand = 1", the user is send all the fragments along with its meta data. Otherwise, just an array of fragments!
-  const userFragments = await Fragment.getAllFragments(req.user, req.query.expand);
+  const userFragments = await Fragment.getAllFragments(ownerId, req.query.expand);
   res?.status(200).json(createSuccessResponse({ fragments: userFragments }));
 };
 
-// // This API get the id and sends the fragment associated with that id!
-// const getFragmentUsingId = (req, res) => {
-//   // Checking the id send by the user!
-//   const { id } = req.params.id;
+// This function separates the fragment id and the requested extension from the query in the url.
+const extractExtensionRequested = (query) => {
+  let fragmentId = query,
+    requestedExtension = '';
+  // Get the index of the "period".
+  const extensionIndex = query.lastIndexOf('.');
+  // If the period is present, update the value of fragmentId and requestedExtension.
+  if (extensionIndex >= 0) {
+    fragmentId = query.substring(0, extensionIndex);
+    requestedExtension = '.' + query.substring(extensionIndex + 1);
+  }
+  // Return the data.
+  return { fragmentId, requestedExtension };
+};
 
-//   // Analysing the id send by the user to check if the entension is included with the id or not!
-//   const { fragmentId, extension } = analyzeIdParam(id);
+// This API get the id and sends the fragment associated with that id!
+const getFragmentUsingId = async (req, res) => {
+  // Get the fragment id send by the user.
+  const id = req.params.id;
 
-//   // Getting the fragment with the id passed by the client!
-//   const fragment = findFragmentWith(fragmentId);
+  // Get the requested extension if passed in the query.
+  const { fragmentId, requestedExtension } = extractExtensionRequested(id);
+  // Get the owner id from the request object.
+  const ownerId = req.user;
 
-//   // If no fragment exist with the passed id, responding with the error message!
-//   if (!fragment) {
-//     res
-//       .status(404)
-//       .json(
-//         createErrorResponse(
-//           404,
-//           `Check the id! There is no fragment stored with the id ${fragmentId}!`
-//         )
-//       );
-//     return;
-//   }
+  // Get the fragments meta data using the fragment id and owner id.
+  const fragmentMetaData = await Fragment.getFragment(ownerId, fragmentId);
 
-//   // Storing the type of the fragment, if there is an extension type passed by the client or using the existing type of the fragment used while storing the fragment!
-//   const fragmentType = extension || getFragmentType(fragment);
+  // Create the fragment object by passing the fragment metadata.
+  const fragment = new Fragment(fragmentMetaData);
 
-//   // Converting the fragment into the required type!
-//   const convertedFragment = convertFragment(fragment, fragmentType);
+  // Return 404, if no fragments are present with the received it.
+  if (!fragment) {
+    res
+      .status(404)
+      .json(
+        createErrorResponse(404, `Check the id! There is no fragment stored with the id ${id}!`)
+      );
+    return;
+  }
 
-//   // If the fragment cannot be converted into the required type, sending the appropriate error message!
-//   if (!convertedFragment) {
-//     res
-//       .status(415)
-//       .json(
-//         createErrorResponse(415, 'The fragment cannot be converted into the extension specified!')
-//       );
-//     return;
-//   }
+  if (requestedExtension) {
+    // Make sure that the request extension is convertable from the current extension.
+    if (fragment.formats.includes(requestedExtension)) {
+      // Call the function to get the data converted into the required format.
+      const fragmentData = await fragment.getConvertedInto(requestedExtension);
+      res.status(200).json(createSuccessResponse({ fragment: fragmentData }));
+    } else
+      res
+        .status(415)
+        .json(
+          createErrorResponse(415, 'The fragment cannot be converted into the extension specified!')
+        );
+    return;
+  }
 
-//   // Finally responding with the converted fragment!
-//   res.status(200).json(createSuccessResponse({ fragment: convertedFragment }));
-// };
+  // Get fragment data.
+  const fragmentData = await fragment.getData();
+  // Send the requested fragments.
+  res.status(200).json(createSuccessResponse({ fragment: fragmentData }));
+};
 
 // This API get the id and sends the metadata of the fragment associated with that id!
 const getFragmentInfoUsingId = async (req, res) => {
@@ -80,6 +100,6 @@ const getFragmentInfoUsingId = async (req, res) => {
 
 module.exports = {
   getFragments,
-  // getFragmentUsingId,
+  getFragmentUsingId,
   getFragmentInfoUsingId,
 };
