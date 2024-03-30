2b9d048d7d30efbfc51b8a0e9cdc45258e7a8d57
Dev Shah <busycaesar@gmail.com>
2024-03-30 09:10:37 -0400

    Feat(Model/data): Create structure for working with AWS s3

---
+++ src/model/data/aws/index.js
@@ -0,0 +1,46 @@
+const MemoryDB = require('./memory-db');
+
+// Create two in-memory databases: one for fragment metadata and the other for raw data
+const data = new MemoryDB();
+const metadata = new MemoryDB();
+
+// Write a fragment's metadata to memory db. Returns a Promise
+const writeFragment = async (ownerId, id, fragment) => await metadata.put(ownerId, id, fragment);
+
+// Read a fragment's metadata from memory db. Returns a Promise
+const readFragment = async (ownerId, id) => await metadata.get(ownerId, id);
+
+// Write a fragment's data buffer to memory db. Returns a Promise
+const writeFragmentData = async (ownerId, id, buffer) => await data.put(ownerId, id, buffer);
+
+// Read a fragment's data from memory db. Returns a Promise
+const readFragmentData = async (ownerId, id) => await data.get(ownerId, id);
+
+// Get a list of fragment ids/objects for the given user from memory db. Returns a Promise
+const listFragments = async (ownerId, expand = false) => {
+  const fragments = await metadata.query(ownerId);
+
+  // If we don't get anything back, or are supposed to give expanded fragments, return
+  if (expand || !fragments) return fragments;
+
+  // Otherwise, map to only send back the ids
+  return fragments.map((fragment) => fragment.id);
+};
+
+// Delete a fragment's metadata and data from memory db. Returns a Promise
+const deleteFragment = async (ownerId, id) =>
+  Promise.all([
+    // Delete metadata
+    metadata.del(ownerId, id),
+    // Delete data
+    data.del(ownerId, id),
+  ]);
+
+module.exports = {
+  listFragments,
+  writeFragment,
+  readFragment,
+  writeFragmentData,
+  readFragmentData,
+  deleteFragment,
+};
