192967a96bdb6479dbe9cdcc9011f36c074c6cf2
Dev Shah <busycaesar@gmail.com>
2024-03-30 09:26:37 -0400

    Add(Model/data/aws): Update s3Client.js to configure with S3SDK to issue commands.

---
+++ src/model/data/aws/s3Client.js
@@ -0,0 +1,52 @@
+// src/model/data/aws/s3Client.js
+
+/**
+ * S3 specific config and objects.
+ */
+const { S3Client } = require('@aws-sdk/client-s3');
+const logger = require('../../../logger');
+
+/**
+ * If AWS credentials are configured in the environment, use them. Normally when we connect to S3 from a deployment in AWS, we won't bother with this. But if you're testing locally, you'll need these, or if you're connecting to LocalStack or MinIO.
+ * @returns Object | undefined
+ */
+const getCredentials = () => {
+  if (process.env.AWS_ACCESS_KEY_ID && process.env.AWS_SECRET_ACCESS_KEY) {
+    const credentials = {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      // Optionally include the AWS Session Token.
+      // Not all situations require this, so we won't check for it above in the if condition, just use it if it is present.
+      sessionToken: process.env.AWS_SESSION_TOKEN,
+    };
+	  
+    logger.debug('Using extra S3 Credentials AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY');
+    return credentials;
+  }
+};
+
+/**
+ * If an AWS S3 Endpoint is configured in the environment, use it.
+ * @returns string | undefined
+ */
+const getS3Endpoint = () => {
+  if (process.env.AWS_S3_ENDPOINT_URL) {
+    logger.debug({ endpoint: process.env.AWS_S3_ENDPOINT_URL }, 'Using alternate S3 endpoint');
+    return process.env.AWS_S3_ENDPOINT_URL;
+  }
+};
+
+/**
+ * Configure and export a new s3Client to use for all API calls.
+ * NOTE: we want to use this client with both AWS S3, but also MinIO and LocalStack in development and testing. We may or may not have various configuration settings, and will pass `undefined` when we don't.
+ */
+module.exports = new S3Client({
+  // The region is always required
+  region: process.env.AWS_REGION,
+  // Credentials are optional (only MinIO needs them, or if you connect to AWS remotely from your laptop)
+  credentials: getCredentials(),
+  // The endpoint URL is optional
+  endpoint: getS3Endpoint(),
+  // We always want to use path style key names
+  forcePathStyle: true,
+});
