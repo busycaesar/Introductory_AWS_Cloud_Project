ecc349172521339098376f805bd7872a80816ca8
Dev Shah <busycaesar@gmail.com>
2024-03-30 09:12:50 -0400

    Chore(Model/data): Add comments in index.js file to explain the code.

--- src/model/data/index.js
+++ src/model/data/index.js
@@ -1,1 +1,4 @@
+// src/model/data/index.js
+
+// Use aws backend service if env variables sets the AWS Region, in-memory otherwise.
 module.exports = process.env.AWS_REGION ? require('./aws') : require('./memory');
