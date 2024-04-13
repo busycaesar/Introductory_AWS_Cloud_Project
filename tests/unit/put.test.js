21171ca6d53ac8c26e039c9d2ce5dd859b1a75f8
Dev Shah <busycaesar@gmail.com>
2024-04-13 11:16:33 -0400

    Add(Unit Test): Update fragment

---
+++ tests/unit/put.test.js
@@ -0,0 +1,64 @@
+const request = require('supertest');
+const fs = require('fs');
+const absolutePath = 'tests/unit/sampleFile';
+const hash = require('../../src/hash');
+const app = require('../../src/app');
+const Fragment = require('../../src/model/fragment');
+
+describe('PUT /fragment', () => {
+  test('Authorized users are able to update the fragment(text/plain)', async () => {
+    const userEmail = 'user1@email.com';
+    // Post a fragment
+    const data = 'This is first fragment data!';
+    // Posting the data along with credentials and supported data type!
+    const response = await request(app)
+        .post('/v1/fragments')
+        .auth(userEmail, 'ps1')
+        .set('Content-Type', 'text/plain')
+        .send(data),
+      // Getting the metadata from of the data posted!
+      fragmentMetaData = response.body.fragment,
+      fragmentId = fragmentMetaData.id;
+    // Make sure the fragment is stored
+    const fragment = new Fragment(fragmentMetaData);
+    expect(await fragment.getData()).toEqual(Buffer.from(data));
+    // Update the fragment
+    const updatedData = 'This is the updated data';
+    const putResponse = await request(app)
+      .put(`/v1/fragments/${fragmentId}`)
+      .auth(userEmail, 'ps1')
+      .set('Content-Type', 'text/plain')
+      .send(updatedData);
+    // Make sure the fragment updated
+    expect(putResponse.status).toBe(201);
+    expect(await fragment.getData()).toEqual(Buffer.from(updatedData));
+  });
+
+  test('Authorized users are able to update the fragment(image/jpeg)', async () => {
+    const userEmail = 'user1@email.com';
+    // Post a fragment
+    const data = fs.readFileSync(`${absolutePath}/wallpaper.jpeg`);
+    // Posting the data along with credentials and supported data type!
+    const response = await request(app)
+        .post('/v1/fragments')
+        .auth(userEmail, 'ps1')
+        .set('Content-Type', 'image/jpeg')
+        .send(data),
+      // Getting the metadata from of the data posted!
+      fragmentMetaData = response.body.fragment,
+      fragmentId = fragmentMetaData.id;
+    // Make sure the fragment is stored
+    const fragment = new Fragment(fragmentMetaData);
+    expect(await fragment.getData()).toEqual(Buffer.from(data));
+    // Update the fragment
+    const updatedData = fs.readFileSync(`${absolutePath}/profile.jpeg`);
+    const putResponse = await request(app)
+      .put(`/v1/fragments/${fragmentId}`)
+      .auth(userEmail, 'ps1')
+      .set('Content-Type', 'image/jpeg')
+      .send(updatedData);
+    // Make sure the fragment updated
+    expect(putResponse.status).toBe(201);
+    expect(await fragment.getData()).toEqual(Buffer.from(updatedData));
+  });
+});
