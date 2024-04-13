330ee3472ae868370f4f8f8c8e2a6d88129d07e6
Dev Shah <busycaesar@gmail.com>
2024-04-13 09:09:08 -0400

    Feat(API): Update fragment

--- src/routes/api/put.js
+++ src/routes/api/put.js
@@ -9,21 +9,56 @@ const logger = require('../../logger');
 // This API updates the fragment associated with the passed id, with the new fragment!
 const updateFragment = async (req, res) => {
   // Checking the id send by the user!
-  const { id } = req.params.id,
+  const { id } = req.params,
     // Checking the type of the fragment requested!
     fragmentType = req.get('Content-Type'),
     // Getting the raw data of the new fragment!
-    fragmentRawData = req.body;
+    fragmentRawData = req.body,
+    ownerId = req.user;
+  let fragmentMetaData, fragment;
 
-  // Getting the fragment associated with the id!
+  try {
+    // Get the fragments meta data using the fragment id and owner id.
+    fragmentMetaData = await Fragment.getFragment(ownerId, id);
 
-  // Making sure that the fragment associated with the id exists! Otherwise responding accordingly!
+    logger.debug({ fragmentMetaData }, `Fragment metadata ${id}`);
+
+    // Getting the fragment associated with the id!
+    fragment = new Fragment(fragmentMetaData);
+  } catch (error) {
+    logger.error(`No fragments found with id ${id}`);
+    res
+      .status(404)
+      .json(
+        createErrorResponse(404, `Check the id! There is no fragment stored with the id ${id}!`)
+      );
+    return;
+  }
 
   // Making sure the requested fragment type is the same as the old one. Otherwise, responding accordingly!
+  if (fragmentType != fragmentMetaData.fragmentType) {
+    logger.error(`Cannot change type of the fragment.`);
+    res
+      .status(415)
+      .json(createErrorResponse(415, `Cannot change type of the fragment to ${fragmentType}!`));
+    return;
+  }
 
-  // Updating the fragment and getting the metadata for the new fragment!
-
-  // Responding success message once the fragment is updated!
+  try {
+    // Update content length
+    const size = parseInt(req.headers['content-length']);
+    fragment.setSize(size);
+    // Updating the fragment and getting the metadata for the new fragment!
+    await fragment.setData(fragmentRawData);
+    // Responding success message once the fragment is updated!
+    res.status(201).json(
+      createSuccessResponse({
+        fragment: fragment.getFragmentMetaData(),
+      })
+    );
+  } catch (error) {
+    res.status(500).json(createErrorResponse(500, `Internal server error`));
+  }
 };
 
 module.exports = { updateFragment };
